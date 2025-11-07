3fae02b40c6a4b8568710324dfc644125647f86c
estevao-s-dev <mail.estevao.s@gmail.com>
2025-11-06 23:31:38 -0300

    desafio-python

---
+++ desafio_python.md
@@ -0,0 +1,38 @@
+Desafio Python
+
+Pedir o nome do aluno.
+
+Solicitar as notas das AOPs:
+
+AOP1 vale de 0 a 1 ponto
+
+AOP2 vale de 0 a 2 pontos
+
+AOP3 vale de 0 a 1 ponto
+
+A soma das três AOPs pode chegar até 4 pontos.
+
+Pedir a nota da Prova Regular, que vale de 0 a 6 pontos.
+
+Calcular o total do aluno no módulo (AOPs + Prova Regular), que pode chegar a 10 pontos no máximo.
+
+Exibir o resultado conforme as regras abaixo:
+
+Se o total for 7 ou mais → o aluno está Aprovado direto 
+
+Se o total for menor que 3 → o aluno está Reprovado direto 
+
+Se o total estiver entre 3 e 7 → o aluno está de Recuperação 
+
+Caso o aluno precise de Recuperação, o programa deve:
+
+Pedir a nota da Prova de Recuperação (de 0 a 10).
+
+Calcular a média final usando a fórmula:
+
+(nota total do módulo + nota da recuperação) / 2
+Se essa média final for 5 ou mais, o aluno fica Aprovado na recuperação 
+
+Caso contrário, fica Reprovado na recuperação 
+
+
---
+++ desafio_python.py
@@ -0,0 +1,80 @@
+' Sistema de notas UVV '
+# libs
+import time
+
+# main
+def main():
+        
+        print("Bem vindo!")
+        nome_aluno = input("Digite o seu nome por favor: ")
+        print("Olá", nome_aluno + "!")
+        
+        notas_aops()
+        nota_prova_regular()
+        analise_das_notas()
+
+# soma de notas das aops
+def notas_aops():
+
+    aop_1 = input("Digite a nota da AOP 1: ")
+    aop_2 = input("Digite a nota da AOP 2: ")
+    aop_3 = input("Digite a nota da AOP 3: ")
+    total_aop = float(aop_1) + float(aop_2) + float(aop_3)
+    
+    if total_aop >= 0 and total_aop <= 4:
+        
+        print("Soma das AOPs:", round(total_aop, 1))
+    
+    else:
+
+        while total_aop > 4 or total_aop < 0:
+    
+            print("Soma de notas inválida. Tente novamente...")
+            aop_1 = input("Digite a nota da AOP 1: ")
+            aop_2 = input("Digite a nota da AOP 2: ")
+            aop_3 = input("Digite a nota da AOP 3: ")
+            total_aop = float(aop_1) + float(aop_2) + float(aop_3)
+        
+
+        
+            if total_aop >= 0 and total_aop <= 4:
+                
+                print("Soma das AOPs:", round(total_aop, 1))
+                break
+
+            else:
+                
+                continue
+
+
+def nota_prova_regular():
+    
+    prova_regular = float(input("Digite a nota da prova regular: "))
+    
+    if prova_regular >= 0 and prova_regular <= 6:
+        
+        print("Nota da prova regular:", round(prova_regular, 1))
+    
+    else:
+        
+        while prova_regular < 0 or prova_regular > 6:
+            
+            print("Nota inválida, tente novamente...")
+            prova_regular = float(input("Digite a nota da prova regular: "))
+            
+            if prova_regular >= 0 and prova_regular <= 6:
+                
+                print("Nota na prova regular:", round(prova_regular, 1))
+                break
+            
+            else:
+                
+                continue
+
+# analise das notas e situacao do aluno
+def analise_das_notas():
+
+    def situacao_do_aluno():
+
+
+main()
