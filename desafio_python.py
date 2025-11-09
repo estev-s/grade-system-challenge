24d194b2a83feffa3848c77a2dff2c76c98ae0ab
estevao-s-dev <mail.estevao.s@gmail.com>
2025-11-09 01:12:25 -0300

    concluido

--- desafio_python.py
+++ desafio_python.py
@@ -5,77 +5,115 @@ import time
 # main
 def main():
         
-        print("Bem vindo!")
-        nome_aluno = input("Digite o seu nome por favor: ")
-        print("Olá", nome_aluno + "!")
+        print                   ("Bem vindo!")
+        nome_aluno              = input("Digite o seu nome por favor: ")
+        print                   ("Olá", nome_aluno + "!")
         
-        notas_aops()
-        nota_prova_regular()
-        analise_das_notas()
+        resultado_aops          = notas_aops()
+        resultado_prova_regular = nota_prova_regular()
+        print                   ("Resultado nas AOPs: ", resultado_aops)
+        print                   ("Resultado na Prova Regular: ", resultado_prova_regular)
+
+        status_do_aluno         = analise_das_notas(resultado_aops, resultado_prova_regular, nome_aluno)
+        
+        
 
 # soma de notas das aops
 def notas_aops():
     
     # adicionar condições caso a nota tenha valor invalido
-    aop_1 = input("Digite a nota da AOP 1: ")
-    aop_2 = input("Digite a nota da AOP 2: ")
-    aop_3 = input("Digite a nota da AOP 3: ")
-    total_aop = float(aop_1) + float(aop_2) + float(aop_3)
-    
-    if total_aop >= 0 and total_aop <= 4:
         
-        print("Soma das AOPs:", round(total_aop, 1))
+    aop_1 = float(input("Digite a nota da AOP 1: "))
+
+    while aop_1 < 0 or aop_1 > 1:
+
+        print   ("Valor inválido, digite novamente...")
+        aop_1   = float(input("Digite a nota da AOP 1: "))
     
-    else:
-
-        while total_aop > 4 or total_aop < 0:
+    aop_2 = float(input("Digite a nota da AOP 2: "))
     
-            print("Soma de notas inválida. Tente novamente...")
-            aop_1 = input("Digite a nota da AOP 1: ")
-            aop_2 = input("Digite a nota da AOP 2: ")
-            aop_3 = input("Digite a nota da AOP 3: ")
-            total_aop = float(aop_1) + float(aop_2) + float(aop_3)
+    while aop_2 < 0 or aop_2 > 2:
         
+        print   ("Valor inválido, digite novamente...")
+        aop_2   = float(input("Digite a nota da AOP 2: "))
+    
 
+    aop_3 = float(input("Digite a nota da AOP 3: "))
+    
+    while aop_3 < 0 or aop_3 > 1:
         
-            if total_aop >= 0 and total_aop <= 4:
-                
-                print("Soma das AOPs:", round(total_aop, 1))
-                break
+        print   ("Valor inválido, digite novamente...")
+        aop_3   = float(input("Digite a nota da AOP 3: "))
+    
+    #total das aops:
 
-            else:
-                
-                continue
+    total_aops  = aop_1 + aop_2 + aop_3
+    print       ("Soma das AOPs:", round(total_aops, 1))
+    return      total_aops
 
 
 def nota_prova_regular():
     
     prova_regular = float(input("Digite a nota da prova regular: "))
+
     
     if prova_regular >= 0 and prova_regular <= 6:
         
-        print("Nota da prova regular:", round(prova_regular, 1))
+        print   ("Nota da prova regular:", round(prova_regular, 1))
+        return  prova_regular
     
     else:
         
         while prova_regular < 0 or prova_regular > 6:
             
-            print("Nota inválida, tente novamente...")
-            prova_regular = float(input("Digite a nota da prova regular: "))
+            print           ("Nota inválida, tente novamente...")
+            prova_regular   = float(input("Digite a nota da prova regular: "))
             
             if prova_regular >= 0 and prova_regular <= 6:
                 
-                print("Nota na prova regular:", round(prova_regular, 1))
+                print       ("Nota na prova regular:", round(prova_regular, 1))
+                return      prova_regular
                 break
             
             else:
                 
                 continue
 
-# analise das notas e situacao do aluno
-def analise_das_notas():
+def analise_das_notas(resultado_aops, resultado_prova_regular, nome_aluno):
+    
+    x = resultado_aops
+    y = resultado_prova_regular
+    z = x + y
+    
+    if z >= 7:
+        
+        print   (nome_aluno, "- Aprovado Direto")
+    
+    if z < 3:
+        
+        print   (nome_aluno, "- Reprovado Direto")
 
-    def situacao_do_aluno():
+    if z >= 3 and z < 7:
+        
+        print   (nome_aluno, "- Em Recuperacao")
+
+        nota_prova_recuperacao = float(input("Digite a nota na prova de recuperacao: "))
+        
+        while nota_prova_recuperacao < 0 or nota_prova_recuperacao > 10:
+            
+            print                   ("Nota invalida, digite novamente...")
+            nota_prova_recuperacao  = float(input("Digite a nota na prova de recuperacao: "))
+        
+        
+        resultado_geral = (x + y + nota_prova_recuperacao) / 2
+        
+        if resultado_geral >= 5:
+
+            print(nome_aluno, "- Aprovado na Recuperacao")
+        
+        else:
+
+            print(nome_aluno, "- Reprovado na Recuperacao")
 
 
 main()
